88e1bee5b1f81f91e66fe1bc0bdac08294f6c2d5
Yiays <yesiateyoursheep@gmail.com>
2022-05-12 12:52:23 +0000

    Anagrams: Highscore support added Also made the highscore system generic

--- common.js
+++ common.js
@@ -46,6 +46,36 @@ function shuffle(array) {
   return array;
 }
 
+function submit_highscore(scope, score, username=null, secret=null, silent=false) {
+  if(username==null) username = prompt("Username");
+  if(secret==null) secret = prompt("Secret code (must be typed by the teacher)");
+
+  $.get(`https://highscore.yiays.workers.dev/?secret=${secret}&scope=${scope}&username=${username}&score=${score}`)
+  .done((data) => { if(!silent) alert(data); })
+  .fail(() => {alert("Something went wrong! Failed to save your highscore.")});
+}
+
+function get_leaderboard(scope, result_converter=null) {
+  $('#board>tbody').html('<tr><td colspan=3>Loading...</td></tr>')
+  $.get(`https://highscore.yiays.workers.dev/?scope=${scope}`)
+  .done((data) => {
+    var entries = Object.keys(data).map((key) => {
+      return [key, result_converter? result_converter(data[key]): data[key]+'%'];
+    });
+    entries.sort((a, b) => {
+      return b[1] - a[1];
+    });
+
+    $('#board>tbody').html('');
+    let i = 0;
+    entries.slice(0, 10).forEach(entry => {
+      $('#board>tbody').append(`<tr><td>${++i}</td><td>${entry[0]}</td><td>${entry[1]}</td></tr>`);
+    });
+    if(i==0) $('#board>tbody').html('<tr><td>--</td><td>-----</td><td>---</td></tr>');
+  })
+  .fail(() => {alert("Failed to get the leaderboard!")});
+}
+
 // Shake plugin for JQuery - https://github.com/ninty9notout/jquery-shake
 (function(d){d.fn.shake=function(a){"function"===typeof a&&(a={callback:a});a=d.extend({direction:"left",distance:20,times:3,speed:140,easing:"swing"},a);return this.each(function(){var b=d(this),k={position:b.css("position"),top:b.css("top"),bottom:b.css("bottom"),left:b.css("left"),right:b.css("right")};b.css("position","relative");var c="up"==a.direction||"down"==a.direction?"top":"left",e="up"==a.direction||"left"==a.direction?"pos":"neg",f={},g={},h={};f[c]=("pos"==e?"-=":"+=")+a.distance;g[c]=
 ("pos"==e?"+=":"-=")+2*a.distance;h[c]=("pos"==e?"-=":"+=")+2*a.distance;b.animate(f,a.speed,a.easing);for(c=1;c<a.times;c++)b.animate(g,a.speed,a.easing).animate(h,a.speed,a.easing);b.animate(g,a.speed,a.easing).animate(f,a.speed/2,a.easing,function(){b.css(k);a.callback&&a.callback.apply(this,arguments)})})}})(jQuery);
